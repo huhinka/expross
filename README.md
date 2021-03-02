b05d4db5baf38503b372a2b49822da1edfcc13a3
huhinka <huhinka@163.com>
2021-03-02 21:57:48 +0800

    add app test

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Expross is a Web Framework, a fake of Express.js using ES6.
 
 Inspired by [expross](https://github.com/WangZhechao/expross).
+
+## Feature
+
+* ES6
+* TDD
---
+++ test/application.test.js
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import expross from '../index.js'
+
+describe('app', () => {
+  it('should be callable', () => {
+    const app = expross()
+    expect(typeof app).toBe('function')
+  })
+})
+
+describe('app.get', () => {
+  it('should return 200 with "hello world"', async done => {
+    const app = expross()
+    app.get('/', async (req, res) => {
+      res.send('hello world')
+    })
+
+    const res = await request(app).get('/')
+
+    expect(res.status).toBe(200)
+    expect(res.text).toBe('hello world')
+
+    done()
+  })
+})
